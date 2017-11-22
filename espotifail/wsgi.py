272341e6d7a9f39d28403805a85bb59aa9f9d7da
luisdanielgp <luisdaniel.guzmanp@gmail.com>
2017-11-21 20:40:17 -0600

    aws setup in new branch

--- espotifail/wsgi.py
+++ espotifail/wsgi.py
@@ -8,6 +8,13 @@ https://docs.djangoproject.com/en/1.11/howto/deployment/wsgi/
 """
 
 import os
+from dotenv import load_dotenv
+try:
+    dotenv_path = os.path.join(os.path.dirname(os.path.dirname(os.path.abspath(__file__))), '.env')
+    load_dotenv(dotenv_path)
+except:
+    pass
+
 
 from django.core.wsgi import get_wsgi_application
 from whitenoise.django import DjangoWhiteNoise
